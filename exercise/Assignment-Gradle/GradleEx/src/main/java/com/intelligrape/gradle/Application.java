900271b78de8e011304b64c41bf952ac6c0b9bfe
gurvinderbhamra <gurvinder.singh@tothenew.com>
2019-03-06 23:52:11 +0530

    Gradle Assignment Added

---
+++ exercise/Assignment-Gradle/GradleEx/src/main/java/com/intelligrape/gradle/Application.java
@@ -0,0 +1,7 @@
+package com.intelligrape.gradle;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello.. welcome to java gradle");
+    }
+}
---
+++ exercise/Assignment-Gradle/GradleEx/src/main/java2/com/intelligrape/gradle/Application2.java
@@ -0,0 +1,7 @@
+package com.intelligrape.gradle;
+
+public class Application2 {
+    public static void main(String[] args) {
+        System.out.println("Hello.. this is java 2 application file");
+    }
+}
